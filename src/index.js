cfee1c214403778d060d3f83a02b6b069eebd542
RomanPiat <108830304+RomanPiat@users.noreply.github.com>
2023-02-06 14:51:07 +0200

    end correct

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ let input ='';
 refs.countryInp.addEventListener('input', debounce((evt) => {
 input = evt.target.value.trim(" ");
 refs.listEl.innerHTML = '';
+console.log(input)
+if (input =="") return
 
 fetchCountries(URL_COUNTRIES, input)
 .then(renderCountryCard)
